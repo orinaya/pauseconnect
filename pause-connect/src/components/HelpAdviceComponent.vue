a99602bad6ef24b2ada442525547ad9c2b466a15
Margouillat <erwanduchene@gmail.com>
2023-12-12 22:04:33 +0100

    Conseils composants, nouvelle branche pour ne pas tout casser demain

---
+++ pause-connect/src/components/HelpAdviceComponent.vue
@@ -0,0 +1,36 @@
+<script setup>
+const advice = defineProps({
+    headline: {
+        type: String,
+    },
+    text: {
+        type: String,
+    },
+});
+</script>
+<template>
+    <div class="advice">
+        <div>
+            <h3>{{ advice.headline }}</h3>
+            <p>{{ advice.text }}</p>
+        </div>
+    </div>
+</template>
+<style scoped>
+.advice {
+    background-color: hsl(206, 66%, 90%);
+    display: flex;
+    gap: 0.8rem;
+}
+
+.advice::before {
+    content: url(../assets/icons/tickMark.svg);
+    display: block;
+    width: 34px;
+    height: 34px;
+}
+
+h3 {
+    font-size: var(--p-text);
+}
+</style>
--- pause-connect/src/views/HelpView.vue
+++ pause-connect/src/views/HelpView.vue
@@ -1,2 +1,17 @@
-<script setup></script>
-<template><h1>HELP</h1></template>
+<script setup>
+import AdviceComponent from "@/components/HelpAdviceComponent.vue";
+const adivicesTab = [
+    {
+        title: "prout",
+        text: "caca",
+    },
+    {
+        title: "pipi",
+        text: "fesse",
+    },
+];
+</script>
+<template>
+    <h1>HELP</h1>
+    <AdviceComponent v-for="advice in adivicesTab" :headline="advice.title" :text="advice.text" />
+</template>
