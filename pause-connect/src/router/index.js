463cf7f116e89f3784f034755ce73fcaf9563188
Margouillat <erwanduchene@gmail.com>
2023-12-13 20:32:06 +0100

    router toujours retour haut de page

--- pause-connect/src/router/index.js
+++ pause-connect/src/router/index.js
@@ -7,39 +7,45 @@ import HelpView from "../views/HelpView.vue";
 import ContactView from "../views/ContactView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomeView,
+        },
+        {
+            path: "/informations",
+            name: "informations",
+            component: InfosView,
+        },
+        {
+            path: "/notre-quiz",
+            name: "quiz",
+            component: QuizView,
+        },
+        {
+            path: "/nos-defis",
+            name: "defis",
+            component: ChallengesView,
+        },
+        {
+            path: "/aide",
+            name: "aide",
+            component: HelpView,
+        },
+        {
+            path: "/contact",
+            name: "contact",
+            component: ContactView,
+        },
+    ],
+
+    //permet de toujours scroll en haut de la page en cas de routerlink
+    scrollBehavior(to, from, savedPosition) {
+        // always scroll to top
+        return { top: 0 };
     },
-    {
-      path: "/informations",
-      name: "informations",
-      component: InfosView,
-    },
-    {
-      path: "/notre-quiz",
-      name: "quiz",
-      component: QuizView,
-    },
-    {
-      path: "/nos-defis",
-      name: "defis",
-      component: ChallengesView,
-    },
-    {
-      path: "/aide",
-      name: "aide",
-      component: HelpView,
-    },
-    {
-      path: "/contact",
-      name: "contact",
-      component: ContactView,
-    },
-  ],
 });
 
 export default router;
