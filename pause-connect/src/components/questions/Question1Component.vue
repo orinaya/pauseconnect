e08dc5d83f03536530412ef20a46ea1cf9f0faae
Margouillat <erwanduchene@gmail.com>
2023-12-13 16:34:08 +0100

    le quizz fonctionne

---
+++ pause-connect/src/components/questions/Question1Component.vue
@@ -0,0 +1,85 @@
+<script setup>
+import ButtonComponent from "@/components/ButtonComponent.vue";
+import { ref } from "vue";
+var answer = ref(0);
+var btnToggle = ref(false);
+</script>
+<template>
+    <div class="question-container">
+        <h2>Combien d’heures passez vous chaque jour sur votre téléphone ?</h2>
+        <div class="question">
+            <div @click="(answer = 1), (btnToggle = true)">
+                <label for="a1">
+                    <div class="answerImg"><img src="../../assets/img/q1_img1.svg" /></div>
+                    <p>0 - 2 heures</p>
+                </label>
+
+                <input id="a1" type="radio" name="answersRadio" class="answerRadio" />
+            </div>
+            <div @click="(answer = 4), (btnToggle = true)">
+                <label for="a2">
+                    <div class="answerImg"><img src="../../assets/img/q1_img2.svg" /></div>
+                    <p>2 - 4 heures</p>
+                </label>
+
+                <input id="a2" type="radio" name="answersRadio" class="answerRadio" />
+            </div>
+            <div @click="(answer = 7), (btnToggle = true)">
+                <label for="a3">
+                    <div class="answerImg"><img src="../../assets/img/q1_img3.svg" /></div>
+                    <p>4 - 7 heures</p>
+                </label>
+                <input id="a3" type="radio" name="answersRadio" class="answerRadio" />
+            </div>
+            <div @click="(answer = 10), (btnToggle = true)">
+                <label for="a4">
+                    <div class="answerImg"><img src="../../assets/img/q1_img4.svg" /></div>
+                    <p>plus de 7 heures</p>
+                </label>
+
+                <input id="a4" type="radio" name="answersRadio" class="answerRadio" />
+            </div>
+        </div>
+        <ButtonComponent
+            @click="$emit('nextQuestion', answer)"
+            class="questionBtn"
+            :class="{ active: btnToggle == false }"
+            color="primary"
+            title="Valider ma réponse"
+        />
+    </div>
+</template>
+<style>
+.question {
+    display: flex;
+    justify-content: space-evenly;
+    gap: 10%;
+}
+
+.question > div {
+    text-align: center;
+}
+
+.answerImg {
+    background-color: var(--indigo-900);
+    border: 2px var(--indigo-500) solid;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    border-radius: 2px;
+    aspect-ratio: 1;
+    &:hover {
+        border-color: var(--indigo-200);
+        cursor: pointer;
+    }
+}
+
+.answerImg img {
+    height: 100%;
+    display: block;
+}
+
+.active {
+    opacity: 0;
+}
+</style>
--- pause-connect/src/views/QuizView.vue
+++ pause-connect/src/views/QuizView.vue
@@ -1,25 +1,63 @@
 <script setup>
 import BannerComponent from "../components/BannerComponent.vue";
+import Question1 from "@/components/questions/Question1Component.vue";
+import { ref } from "vue";
 const previousPages = [
-  {
-    path: "/",
-    name: "Accueil",
-  },
+    {
+        path: "/",
+        name: "Accueil",
+    },
 ];
 const item = {
-  title: "Quiz : Es-tu addict aux écrans et aux réseaux sociaux ?",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tellus tellus, lacinia non tellus at, convallis laoreet felis. Fusce est lorem, scelerisque vel enimeget, pellentesque vestibulum massa.",
+    title: "Quiz : Es-tu addict aux écrans et aux réseaux sociaux ?",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tellus tellus, lacinia non tellus at, convallis laoreet felis. Fusce est lorem, scelerisque vel enimeget, pellentesque vestibulum massa.",
 };
 
 import image from "../assets/img/challenge-banner.png";
+
+var questionIndex = ref(1);
+var response = ref(0);
+
+function addIndex(points) {
+    questionIndex.value++;
+    response = response + points;
+}
 </script>
 
 <template>
-  <BannerComponent
-    :previousPages="previousPages"
-    pageName="Notre quiz"
-    :title="item.title"
-    :text="item.text"
-    :image="image"
-  />
+    <pre>Points: {{ response }} Index:{{ questionIndex }}</pre>
+    <section id="quiz-questions">
+        <Question1 v-if="questionIndex == 1" v-on:nextQuestion="addIndex" />
+    </section>
 </template>
+<style>
+#quiz-questions {
+    height: 65vh;
+    min-height: fit-content;
+    max-width: 1280px;
+    box-shadow: 0px 4px 22.1px 0px rgba(0, 0, 0, 0.25);
+    margin: 80px auto;
+    border-radius: 4px;
+    padding: 30px;
+    box-sizing: border-box;
+}
+
+.question-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+}
+
+.questionBtn {
+    align-self: end;
+}
+
+/* .answerRadio {
+    display: none;
+}
+
+.answerRadio:checked ~ label {
+    border: 6px solid black;
+} */
+</style>
