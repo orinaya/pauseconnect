66dcd7bdd606f6fa60155379ff6f41a98e8e895a
Margouillat <erwanduchene@gmail.com>
2023-12-13 12:07:05 +0100

    Numbers component stats and shit

--- pause-connect/src/views/HomeView.vue
+++ pause-connect/src/views/HomeView.vue
@@ -2,16 +2,20 @@
 import HeroComponent from "@/components/HeroComponent.vue";
 import SmallMap from "@/components/SmallMap.vue";
 import QuizLinkComponent from "@/components/QuizLinkComponent.vue";
+import NumbersComponent from "@/components/NumbersComponent.vue";
 </script>
 
 <template>
-  <main>
-    <HeroComponent /> <QuizLinkComponent />
-    <SmallMap
-      title="Sauras-tu relever nos défis ?"
-      text="Nous vous avons préparé une liste de défis pour vaincre votre addiction ou pour continuer vos efforts vers votre réussite.    "
-      img-desktop="src/assets/img/smallMap.svg"
-      img-mobile="src/assets/img/smallMapMobile.svg"
-    />
-  </main>
+    <main>
+        <HeroComponent />
+        <NumbersComponent :-link-show="true" />
+        <QuizLinkComponent />
+
+        <SmallMap
+            title="Sauras-tu relever nos défis ?"
+            text="Nous vous avons préparé une liste de défis pour vaincre votre addiction ou pour continuer vos efforts vers votre réussite.    "
+            img-desktop="src/assets/img/smallMap.svg"
+            img-mobile="src/assets/img/smallMapMobile.svg"
+        />
+    </main>
 </template>
--- pause-connect/src/views/InfosView.vue
+++ pause-connect/src/views/InfosView.vue
@@ -1,15 +1,15 @@
 <script setup>
 import BannerComponent from "../components/BannerComponent.vue";
 const previousPages = [
-  {
-    path: "/",
-    name: "Accueil",
-  },
+    {
+        path: "/",
+        name: "Accueil",
+    },
 ];
 
 const item = {
-  title: "Qu’est-ce que l’addiction aux écrans et aux réseaux sociaux ?",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tellus tellus, lacinia non tellus at, convallis laoreet felis. Fusce est lorem, scelerisque vel enimeget, pellentesque vestibulum massa.",
+    title: "Qu’est-ce que l’addiction aux écrans et aux réseaux sociaux ?",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tellus tellus, lacinia non tellus at, convallis laoreet felis. Fusce est lorem, scelerisque vel enimeget, pellentesque vestibulum massa.",
 };
 
 import image from "../assets/img/infos-banner.png";
@@ -18,18 +18,20 @@ import AddictionsComponent from "@/components/AddictionsComponent.vue";
 import SourcesComponent from "@/components/SourcesComponent.vue";
 import PeopleComponent from "@/components/PeopleComponent.vue";
 import ConsequencesComponent from "@/components/ConsequencesComponent.vue";
+import NumbersComponent from "@/components/NumbersComponent.vue";
 </script>
 <template>
-  <BannerComponent
-    :previousPages="previousPages"
-    pageName="Informations"
-    :title="item.title"
-    :text="item.text"
-    :image="image"
-  />
-  <PeopleComponent />
-  <SignComponent />
-  <ConsequencesComponent />
-  <AddictionsComponent />
-  <SourcesComponent />
+    <BannerComponent
+        :previousPages="previousPages"
+        pageName="Informations"
+        :title="item.title"
+        :text="item.text"
+        :image="image"
+    />
+    <NumbersComponent />
+    <PeopleComponent />
+    <SignComponent />
+    <ConsequencesComponent />
+    <AddictionsComponent />
+    <SourcesComponent />
 </template>
